fb9d63c241423823df09dd2685f68a8b0cc3fa5d
DeveloperOnCall <admin@developeroncall.com>
2021-06-24 12:05:05 -0400

    Tweaks

--- Http/Middleware/GenerateMenus.php
+++ Http/Middleware/GenerateMenus.php
@@ -23,6 +23,7 @@
 namespace Modules\Article\Http\Middleware;
 
 use Closure;
+use Modules\Modulemanager\Entities\MModule;
 
 class GenerateMenus
 {
@@ -36,52 +37,58 @@ class GenerateMenus
      */
     public function handle($request, Closure $next)
     {
-        \Menu::make('admin_sidebar', function ($menu) {
 
-            // Articles Dropdown
-            $articles_menu = $menu->add('<i class="c-sidebar-nav-icon fas fa-file-alt"></i> Article', [
-                'class' => 'c-sidebar-nav-dropdown',
-            ])
-            ->data([
-                'order'         => 81,
-                'activematches' => [
-                    'admin/posts*',
-                    'admin/categories*',
-                ],
-                'permission' => ['view_posts', 'view_categories'],
-            ]);
-            $articles_menu->link->attr([
-                'class' => 'c-sidebar-nav-dropdown-toggle',
-                'href'  => '#',
-            ]);
+        if( \Module::has('Article')) {
 
-            // Submenu: Posts
-            $articles_menu->add('Posts', [
-                'route' => 'backend.posts.index',
-                'class' => 'c-sidebar-nav-item',
-            ])
-            ->data([
-                'order'         => 82,
-                'activematches' => 'admin/posts*',
-                'permission'    => ['edit_posts'],
-            ])
-            ->link->attr([
-                'class' => "c-sidebar-nav-link",
-            ]);
-            // Submenu: Categories
-            $articles_menu->add(' Categories', [
-                'route' => 'backend.categories.index',
-                'class' => 'c-sidebar-nav-item',
-            ])
-            ->data([
-                'order'         => 83,
-                'activematches' => 'admin/categories*',
-                'permission'    => ['edit_categories'],
-            ])
-            ->link->attr([
-                'class' => "c-sidebar-nav-link",
-            ]);
-        })->sortBy('order');
+            \Menu::make('admin_sidebar', function ($menu) {
+
+                    // Articles Dropdown
+                    $articles_menu = $menu->add('<i class="c-sidebar-nav-icon fas fa-file-alt"></i> Article', [
+                        'class' => 'c-sidebar-nav-dropdown',
+                    ])
+                        ->data([
+                            'order'         => 81,
+                            'activematches' => [
+                                'admin/posts*',
+                                'admin/categories*',
+                            ],
+                            'permission' => ['view_posts', 'view_categories'],
+                        ]);
+                    $articles_menu->link->attr([
+                        'class' => 'c-sidebar-nav-dropdown-toggle',
+                        'href'  => '#',
+                    ]);
+
+                    // Submenu: Posts
+                    $articles_menu->add('Posts', [
+                        'route' => 'backend.posts.index',
+                        'class' => 'c-sidebar-nav-item',
+                    ])
+                        ->data([
+                            'order'         => 82,
+                            'activematches' => 'admin/posts*',
+                            'permission'    => ['edit_posts'],
+                        ])
+                        ->link->attr([
+                            'class' => "c-sidebar-nav-link",
+                        ]);
+                    // Submenu: Categories
+                    $articles_menu->add(' Categories', [
+                        'route' => 'backend.categories.index',
+                        'class' => 'c-sidebar-nav-item',
+                    ])
+                        ->data([
+                            'order'         => 83,
+                            'activematches' => 'admin/categories*',
+                            'permission'    => ['edit_categories'],
+                        ])
+                        ->link->attr([
+                            'class' => "c-sidebar-nav-link",
+                        ]);
+                })->sortBy('order');
+
+        }
+
 
         return $next($request);
     }
